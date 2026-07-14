a990dd7e1b86503c3e30d883105b568c0e81c754
Ankit Kumar Rai <ankitdsa2023@gmail.com>
2026-07-15 01:05:54 +0530

    feat: Folder structure setup

---
+++ ecosystem.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+    apps: [
+        {
+            name: "backend-template-mongodb",
+            script: "./dist/server.js",
+            instances: 1,
+            exec_mode: "fork",
+
+            watch: false,
+
+            autorestart: true,
+
+            max_memory_restart: "512M",
+
+            env: {
+                NODE_ENV: "development"
+            },
+
+            env_production: {
+                NODE_ENV: "production"
+            }
+        }
+    ]
+};
+
+
+
+// pm2 start ecosystem.config.js --env production
